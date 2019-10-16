092ac10d66b63acbf639c692a9a595962a4d656a
Marcielo Bell <marcielobell@gmail.com>
2019-10-16 17:42:17 -0600

    Welcome screen

--- client/src/components/Onboarding.js
+++ client/src/components/Onboarding.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import OnboardingWelcome from './OnboardingWelcome.js';
 
 const Onboarding = (props) => {
   return (
       <div>
-          Onboarding
+        {/* if else switching routes (displays each onboarding component) */}
+        <p1>First Page</p1>
+          <OnboardingWelcome />
+
       </div>
   )
 }
