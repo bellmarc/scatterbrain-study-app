7481aebd9a91545c380defccc01c3478cdc2f197
fromjdobson <fromjdobson@gmail.com>
2019-10-15 19:34:24 -0600

    setup basic switch route in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import './style/App.css';
-
+import { Switch, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Onboarding from './components/Onboarding';
+import TopicsList from './components/TopicsList';
+import InSession from './components/InSession';
+import Settings from './components/Settings';
+import Help from './components/Help';
 
 function App() {
     return (
         <div>
-            App
+            <Navbar />
+            <Switch>
+                <Route path="/topics" component={ TopicsList }/>                
+                <Route path="/session" component={ InSession }/>
+                <Route path="/settings" component={ Settings }/>
+                <Route path="/help" component={ Help }/>
+                <Route path="/" component={ Onboarding }/>
+            </Switch>
         </div>
     )
 }
