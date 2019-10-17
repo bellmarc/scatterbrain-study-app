5f1b178151ea5eef2f52da09f2822aeb2c923e96
fromjdobson <fromjdobson@gmail.com>
2019-10-17 14:47:36 -0600

    made basic user getter and setter functions for server and localstorage

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {userSessionContext} from '../Providers/UserSession/UserSession'
 
 const Landing = (props) => {
+  const user = useContext(userSessionContext)
+  
   return (
       <div>
           Landing
+          <button onClick={
+            () => {
+              user.addNewUserToServer({name:'Jonathan', interests: "dogs"}).then(
+                newUser => {
+                  console.log(newUser)
+                }
+              )
+            }
+          }>Add User Jonathan</button>
+          <button onClick={
+            () => {
+              user.getUserFromServerById('5da8c7d2bf600724161ab151').then(
+                user => {
+                  console.log(user)
+                }
+              )
+            }
+          }>Get Users</button>
+          <button onClick={
+            () =>{
+              console.log(user.getUserFromBrowser())
+            }
+          }>Get LocalStorage</button>
+          <button onClick={
+            () => {
+              user.addUserToBrowser({user:"Randon"})
+            }
+          }>Set User in LocalStorage</button>
       </div>
   )
 }
--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import App from './App.js';
+import UserSessionProvider from './Providers/UserSession/UserSession';
 
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
+    <UserSessionProvider>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </UserSessionProvider>,
     document.getElementById('root')
 )
