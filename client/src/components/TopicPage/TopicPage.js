423bc8bb2ed8a0929624dfacfc5e80bae20a5001
Dan Clayton <clayton.daniel.j@gmail.com>
2019-10-17 18:47:13 -0600

    merging to master

--- client/src/components/TopicPage/TopicPage.js
+++ client/src/components/TopicPage/TopicPage.js
@@ -1,20 +1,23 @@
-import React, { useState }from 'react';
+import React, { useState, useEffect }from 'react';
 import '../../style/TopicPage/TopicPage.css';
 import TopicList from './TopicList.js';
 import TopicInstructions from './TopicInstructions.js';
 import axios from 'axios';
 import StartSession from './StartSession.js';
-import TopicSelected from './TopicSelected.js'
+import TopicSelected from './TopicSelected.js';
+import userBrowser from '../../functions/User/Browser.js';
 
 //user for test, will be from context
-const currentUser = {
-    name: 'Bobby',
-    userId: '5da7408bf1f54a4e0898b990'
-}
+// const currentUser = {
+//     name: 'Bobby',
+//     userId: '5da7408bf1f54a4e0898b990'
+// }
 
 function TopicPage(props){
     const [ sessionConfirm, setSessionConfirm ] = useState({topic: {}, isStarting: false});
     const [ topicSelect, setTopicSelect ] = useState({topic: {}, isSelected: false});
+    const currentUser = userBrowser.get()
+
 
     const handleStart = topicIdtoStart => {
         //needs route
@@ -49,6 +52,7 @@ function TopicPage(props){
                     topic= {topicSelect.topic} 
                     setSessionConfirm = {setSessionConfirm}
                     handleGoBack = {handleGoBack}/>}
+                    {/* <button onClick= {() => userBrowser.add(currentUser)}>Magic Button</button> */}
         </main>
     )
 }
