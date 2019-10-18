f7ab5709645460155207aee81d881576068900b5
Marcielo Bell <marcielobell@gmail.com>
2019-10-18 14:29:29 -0600

    working on priority

--- client/src/components/Onboarding/AddPriority.js
+++ client/src/components/Onboarding/AddPriority.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, {useState } from "react";
 import "../../style/Onboarding/AddPriority.css";
+import userBrowser from '../../functions/User/Browser.js';
+import userServer from "../../functions/User/Server.js";
+
 
 
 function AddPriority(props) {
-  console.log(props.match.params)
+  const[priority, setPriority] = useState("")
+
+  const handleChange = (e) => {
+    setPriority(e.target.value)
+  }
+  console.log(props.match.params) //id
   return (
     <div className="add-priority-page">
       <div className="add-priority-text">
@@ -11,8 +19,8 @@ function AddPriority(props) {
         <h5>
           <em>Higher priority (i.e. 5) is more important.</em>
         </h5>
-        <div className="priority-btn-container">
-          <button type="button">
+        <form className="priority-btn-container" onChange={handleChange} value={priority}>
+          <button type="minus-button">
             <b>-</b>
           </button>
           <input
@@ -23,19 +31,20 @@ function AddPriority(props) {
             max="5"
             placeholder="Type your number"
           ></input>
-          <button type="button">
+          <button type="add-button">
             <b>+</b>
           </button>
-        </div>
+        </form>
         <button className="add-priority-btn"
         onClick={()=> {
 
+        userServer.add({ priority: priority})
+        .then(res => console.log(res))
+        .catch(err => console.log(err))
         }}>
         Next
+        {/* .then(props.history.push("/onboard/profile-complete")) */}
         </button>
-         {/* {"/onboard/profile-complete"} >
-          Next */}
-
       </div>
     </div>
   );
