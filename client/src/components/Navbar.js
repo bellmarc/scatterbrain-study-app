9ce9a21e948d82b3e627b656f0f2316521b672a0
Dan Clayton <clayton.daniel.j@gmail.com>
2019-10-21 14:42:16 -0600

    general layout and style/color selection

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -10,17 +10,12 @@ function Navbar() {
     }
     
     return (
-        <>
-        {menu && <Menu close={toggleMenu} speed={200}/>}
-        <ul className="navbar" >
-            <li><Link to = '/'>Landing</Link></li>  
-            <li><Link to = '/onboard'>Onboarding</Link></li>  
-            <li><Link to = '/topics'>Topics</Link></li>  
-            <li><Link to = '/session'>Session</Link></li>  
-            <li><Link to = '/settings'>Settings</Link></li>  
-            <li><button onClick={()=>toggleMenu()}>Show Menu</button></li>  
-        </ul>
-        </>
+        <div className="navbar">
+          <h1 className="navbar-title">Scatterbrain </h1>
+          <h3 className="navbar-subtitle">Spaced Learning Study Tool</h3>
+          <button className = 'menu-button' onClick={()=>toggleMenu()}>Menu</button>
+          {menu && <Menu close={toggleMenu} speed={200}/>}
+        </div>
         )
 }
 
